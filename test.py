0818234acf8474c11d4c1fe1d030dd3e7df24829
sejinlim2001 <65152089+sejinlim2001@users.noreply.github.com>
2025-02-25 14:05:47 +0000

    commit

--- test.py
+++ test.py
@@ -5,5 +5,6 @@ data = csv.reader(f)
 header = next(data)
 print(header)
 for row in data:
+    
     print(row)
 f.close()
--- test3.py
+++ test3.py
@@ -1,3 +1,18 @@
-print(3)
+import csv
 
-name 
+f = open('Daejeon_temp.csv', 'r', encoding='cp949')
+data = csv.reader(f)
+header = next(data)
+print(header)
+max_temp = -999
+max_date = ''
+for row in data:
+    if row[-1] == '':
+        row[-1] = -999
+    row[-1] = float(row[-1])
+    if max_temp < row[-1]:
+        max_temp = row[-1]
+        max_date = row[0]
+f.close()
+
+print("기상 관측 이래, 대전의 최고 기온이 가장 높았던 날은", max_date,"로,", max_temp, "도 였습니다.")
